da766c6f1e00129a520e14e1b28822066306517b
Ricky199X <rickyrojas19@gmail.com>
2020-11-07 11:26:09 -0500

    adds some seed data to DB for articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
 
+    # active record association with comment model
+    has_many :comments, dependent: :destroy
+
+    
     # validates article title and minimum length of title
     validates :title, presence: true,
         length: { minimum: 5}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Article Seed Data
+
+articles = Article.create([
+    {
+        title: 'PS5 Review',
+        text: 'The PS5 is awesome!!!!'
+    },
+    {
+        title: 'Xbox Review',
+        text: 'The Xbox stinks!!!!'
+    }
+])
+
+# Comment Seed Data
+
+comments = Comment.create([
+    {
+        commenter: 'PS5 Boi',
+        body: 'I totally agree man!!!',
+        article: Article.first
+    },
+    {
+        commenter: 'Xbox GamerGurl',
+        body: 'No, u stink!!!',
+        article: Article.last
+    }
+])
