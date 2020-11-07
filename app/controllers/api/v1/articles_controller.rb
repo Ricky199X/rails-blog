8f4032a451c3bfce15c1d7260bb948cffd9d5803
Ricky199X <rickyrojas19@gmail.com>
2020-11-07 14:19:22 -0500

    adds CRUD methods to articles controller + namespaces for api

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,65 @@
+module Api
+    module v1
+        class ArticlesController < ApplicationController
+
+
+            def index
+                articles = Article.all
+                render json: ArticleSerializer.new(articles, options).serialized_json
+            end
+
+            def show
+                article = Article.find(params[:id])
+                render json: ArticleSerializer.new(article, options).serialized_json
+            end
+
+            def new
+                article = Article.new
+            end
+
+            def create
+                article = Article.new(article_params)
+
+                if article.save
+                    render json: ArticleSerializer.new(article).serialized_json
+                else
+                    render json: { error: article.errors.messages }, status: 422
+                end
+            end
+
+            def update
+                article = Article.find(params[:id])
+
+                if article.update(article_params)
+                    render json: ArticleSerializer.new(article, options).serialized_json
+                else
+                    render json: { error: article.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+
+                if article.destroy
+                    head :no_content
+                else
+                    render json: { error: article.errors.messages }, status: 422
+                end
+            end
+
+            private
+            def article_params
+                params.require(:article).permit(:title, :text)
+            end
+
+            # ensures associated comment data for an article is included in the JSON payload
+            def options
+                @options ||= { include: %i[comments] }
+            end
+
+
+        end
+
+
+    end 
+end
---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,6 @@
+module Api
+    module v1
+        class CommentsController < ApplicationController
+        end
+    end 
+end
