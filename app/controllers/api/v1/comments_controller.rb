8fb1013dcd3a88dfa800d3c656325898e4fbff06
Ricky199X <rickyrojas19@gmail.com>
2020-11-07 14:35:15 -0500

    adds create + destroy methods to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,38 @@
 module Api
     module v1
         class CommentsController < ApplicationController
+
+            http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
+            def create
+                article = Article.find(params[:article_id])
+                comment = article.comments.create(comment_params)
+
+                if comment.save
+                    render json: ArticleSerializer.new(article).serialized_json
+                else
+                    render json: { error: comment.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:article_id])
+                comment = article.comments.find(params[:id])
+
+                if comment.destroy
+                    render json: ArticleSerializer.new(article).serialized_json
+                else
+                    render json: { error: comment.errors.messages }, status: 422
+                end
+            end
+
+            private
+            def comment_params
+                params.require(:comment).permit(:commenter, :body)
+            end
+
         end
+
+
     end 
 end
