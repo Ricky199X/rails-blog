2e3eed064f934a7992101ec683860a0d90b823a8
Ricky199X <rickyrojas19@gmail.com>
2020-11-07 17:23:16 -0500

    adds article show component + routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,12 +4,14 @@ import { Route, Switch, Link } from 'react-router-dom'
 // Component Imports
 import Welcome from './Welcome'
 import Articles from './articles/Articles'
+import Article from './articles/Article'
 
 const App = () => {
     return (
         <Switch>
             <Route exact path='/' component={Welcome} />
             <Route exact path='/articles' component={Articles} />
+            <Route exact path='/articles/:id' component={Article} />
         </Switch>
     )
 }
---
+++ app/javascript/components/articles/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Article = () => {
+    return (
+        <div>
+            <h1>This is an article</h1>
+        </div>
+    )
+}
+
+export default Article
