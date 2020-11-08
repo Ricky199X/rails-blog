d74ef40060105785874f52446a6c61bad65a6c0a
Ricky199X <rickyrojas19@gmail.com>
2020-11-08 17:57:33 -0500

    adds loading hook + renders articles information to show page upon load

--- app/javascript/components/article/Article.js
+++ app/javascript/components/article/Article.js
@@ -3,15 +3,10 @@ import axios from 'axios'
 
 const Article = (props) => {
 
-    const [article, setArticle] = useState({
-        title: '',
-        text: ''
-    })
+    const [article, setArticle] = useState({})
 
-    const [comment, setComment] = useState({
-        commenter: '',
-        body: ''
-    })
+    const [comment, setComment] = useState({})
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         const articleId = props.match.params.id
@@ -20,19 +15,31 @@ const Article = (props) => {
         axios.get(url)
             .then(resp => {
                 setArticle(resp.data)
+                setLoaded(true)
             })
             .catch(resp => console.log(resp))
     }, [article.length]);
 
-
-    return (
-        <div className="container">
-            <div className="article"></div>
-            <div className="comments"></div>
-            <div className="form">This form is going here</div>
-            <div className="buttons">The back and edit buttons will be here</div>
-        </div>
-    )
+    return loaded ?
+        (
+            <div className="container">
+                <div className="article">
+                    <strong>Title: </strong>
+                    {article.data.attributes.title}
+                </div>
+                <div className="text">
+                    <strong>Text: </strong>
+                    {article.data.attributes.text}
+                </div>
+                <div className="comments"></div>
+                <div className="form">This form is going here</div>
+                <div className="buttons">The back and edit buttons will be here</div>
+            </div>
+        )
+        :
+        (
+            <h1>Loading!!!</h1>
+        )
 }
 
 export default Article
